c61ce4aa6ac998f8ac95c03479c54040564516e7
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-02-11 20:21:22 -0800

    finished alki object

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var pikePlace = {
+ar pikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
@@ -131,6 +131,42 @@ var bellevueSquare = {
     var ul = document.createElement('ul');
     document.body.appendChild(ul);
     ul.appendChild(document.createTextNode('Bellevue Square'));
+
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.cookiesPurchasedArray[i])));
+      ul.appendChild(li);
+    }
+    var li2 = document.createElement('li');
+    li2.appendChild(document.createTextNode('Total: ' + Math.floor(this.total)));
+    ul.appendChild(li2);
+  },
+};
+
+var alki = {
+  min: 3,
+  max: 24,
+  avg: 2.6,
+  total: 0,
+  timeOfDay: ['Ten; ', 'Eleven: ', 'Twelve: ', 'One: ', 'Two: ', 'Three: ', 'Four: ', 'Five: '],
+  cookiesPurchasedArray: [],
+
+  randCustPerHour: function() {
+    return Math.random() * (this.max - this.min + 1) + this.min;
+  },
+  cookiesPurchased: function(){
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var rand = this.avg * this.randCustPerHour();
+      this.cookiesPurchasedArray.push(rand);
+      this.total += rand;
+    }
+  },
+
+  renderCookiesPerHour: function() {
+    this.cookiesPurchased();
+    var ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    ul.appendChild(document.createTextNode('Alki'));
 
     for (var i = 0; i < this.timeOfDay.length; i++) {
       var li = document.createElement('li');
