47cbd1bab8683662ae44e5190b6d76d4e3b2b19e
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-02-11 20:10:24 -0800

    first two methods working

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var pikesPlace = {
   min: 17,
   max: 88,
-  avgCookieSale: 5.2,
-  randCustPerHour: function (min, max) {
-    return Math.random() * (max - min) + min
-  }
+  avg: 5.2,
+  timeOfDay: ['Ten:', 'Eleven:', 'Twelve:', 'One:', 'Two:', 'Three:', 'Four:', 'Five:'],
+  cookiesPurchasedArray: [],
+
+  randCustPerHour: function() {
+    return Math.random() * (this.max - this.min + 1) + this.min;
+  },
+
+  cookiesPurchased: function() {
+    for ( var i = 0; i < this.timeOfDay.length; i++) {
+      this.cookiesPurchasedArray.push(this.avg * this.randCustPerHour());
+    }
+  },
+  numberCookiesPerHour: function() {
+    for (var i = 0; i < 7; i++) {
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.randCustPerHour(this.min, this.max)*(this.avg))));
+      ul.appendChild(li)
+    }
+
+  },
 }
