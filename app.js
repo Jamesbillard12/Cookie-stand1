b85141177773547783288fee80c32e75479038ab
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-02-21 21:11:22 -0800

    made one table to produce alternate

--- app.js
+++ app.js
@@ -22,54 +22,43 @@ function cookieShop(name, min, max, avg) {
   this.total = 0;
   this.cookiesPurchasedArray = [];
   listOfShops.push(this);
+  this.renderCookiesPerHour();
+}
 //rand customer generator
-  cookieShop.prototype.randCustPerHour = function(){
-    return Math.random() * (this.max - this.min + 1) + this.min;
-  };
+cookieShop.prototype.randCustPerHour = function(){
+  return Math.random() * (this.max - this.min + 1) + this.min;
+};
 //generates cookies and pushes to cookiesPurchasedArray
-  cookieShop.prototype.cookiesPurchased = function() {
-    for (var i = 1; i < headerArr.length - 1; i++) {
-      var rand = Math.round(this.avg * this.randCustPerHour());
-      this.cookiesPurchasedArray.push(rand);
-      this.total += rand;
-    };
+cookieShop.prototype.cookiesPurchased = function() {
+  for (var i = 1; i < headerArr.length - 1; i++) {
+    var rand = Math.round(this.avg * this.randCustPerHour());
+    this.cookiesPurchasedArray.push(rand);
+    this.total += rand;
   };
+};
 
 //this renders the cookie table
-  cookieShop.prototype.renderCookiesPerHour = function() {
-    this.cookiesPurchased();
-    var table = document.getElementById('storeTable');
-    var tr = document.createElement('tr');
-    table.appendChild(tr);
-    var tdNames = document.createElement('td');
-    tdNames.appendChild(document.createTextNode(this.name));
-    tr.appendChild(tdNames);
+cookieShop.prototype.renderCookiesPerHour = function() {
+  this.cookiesPurchased();
+  var table = document.getElementById('storeTable');
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+  var tdNames = document.createElement('td');
+  tdNames.appendChild(document.createTextNode(this.name));
+  tr.appendChild(tdNames);
 
-    for (var i = 0; i < this.cookiesPurchasedArray.length; i++) {
-      var td = document.createElement('td');
-      td.appendChild(document.createTextNode(this.cookiesPurchasedArray[i]));
-      tr.appendChild(td);
-    }
-    var td2 = document.createElement('td');
-    td2.appendChild(document.createTextNode(this.total));
-    tr.appendChild(td2);
-  };
-  //alternates row colors
-  cookieShop.prototype.alternate = function(id){
-    if(document.getElementsByTagName){
-      var table = document.getElementById('storeTable');
-      var rows = table.getElementsByTagName('tr');
-      for(var i = 0; i < rows.length; i++){
-        if(i % 2 === 0){
-          rows[i].className = 'even';
-        }else{
-          rows[i].className = 'odd';
-        }
-      }
-    }
+  for (var i = 0; i < this.cookiesPurchasedArray.length; i++) {
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(this.cookiesPurchasedArray[i]));
+    tr.appendChild(td);
   }
+  var td2 = document.createElement('td');
+  td2.appendChild(document.createTextNode(this.total));
+  tr.appendChild(td2);
+};
+//alternates row colors
 
-}
+
 //created function so i can create more stores
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -81,8 +70,6 @@ function handleFormSubmit(event) {
   var avg = parseInt(event.target.avg.value);
 
   var newCookieShop = new cookieShop(name, min, max, avg);
-  newCookieShop.renderCookiesPerHour();
-  newCookieShop.alternate();
 
   event.target.name.value = null;
   event.target.min.value = null;
@@ -103,24 +90,12 @@ form.addEventListener('submit', handleFormSubmit);
 //where methods and functions are called
 
 var pikePlace = new cookieShop('Pike-Place ', 17, 88, 5.2);
-pikePlace.renderCookiesPerHour();
-pikePlace.alternate();
-
 var seaTacAir = new cookieShop('Sea Tac Airport ', 6, 24, 1.2);
-seaTacAir.renderCookiesPerHour();
-seaTacAir.alternate();
-
 var southCenter = new cookieShop('South Center ', 11, 38, 1.9);
-southCenter.renderCookiesPerHour();
-southCenter.alternate();
-
 var bellevueSquare = new cookieShop('Bellevue Square ', 20, 48, 3.3);
-bellevueSquare.renderCookiesPerHour();
-bellevueSquare.alternate();
-
 var alki = new cookieShop('Alki ', 3, 24, 2.6);
-alki.renderCookiesPerHour();
-alki.alternate();
+
+
 
 var getTotals = function(){
 
@@ -128,7 +103,7 @@ var getTotals = function(){
   // Add ID to the totals row for later access (and styling)
   totalsTR.id = 'totalsTR';
 
-  totalTable.appendChild(totalsTR);
+  storeTable.appendChild(totalsTR);
   var totalByTimeOfDayDescTD = document.createElement('td');
   totalByTimeOfDayDescTD.textContent = 'Totals'
   totalsTR.appendChild(totalByTimeOfDayDescTD);
@@ -143,14 +118,29 @@ var getTotals = function(){
     totalsTR.appendChild(totalByTimeOfDayTD);
   }
 
-    var grandTotalTD = document.createElement('td');
+  var grandTotalTD = document.createElement('td');
 
-    var grandTotal = 0;
-    for (i = 0; i < listOfShops.length; i++) {
-      grandTotal += listOfShops[i].total;
+  var grandTotal = 0;
+  for (var i = 0; i < listOfShops.length; i++) {
+    grandTotal += listOfShops[i].total;
+  }
+
+  grandTotalTD.textContent = grandTotal;
+  totalsTR.appendChild(grandTotalTD);
+
+  var alternate = function(){
+    if(document.getElementsByTagName){
+      var table = document.getElementById('storeTable');
+      var rows = table.getElementsByTagName('tr');
+      for(var i = 0; i < rows.length; i++){
+        if(i % 2 === 0){
+          rows[i].className = 'even';
+        }else{
+          rows[i].className = 'odd';
+        }
+      }
     }
-
-    grandTotalTD.textContent = grandTotal;
-    totalsTR.appendChild(grandTotalTD);
+  }
+  alternate();
 }
 getTotals();
